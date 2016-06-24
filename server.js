e9d2187fb4a9ac7c7347604f5e490d396da9c3c4
Traverse Clayton <Traverse Clayton>
2016-06-24 12:30:04 -0700

    commit

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Local change in VS Code</h1>');
+    res.end('<h1>Another change in VS Code</h1>');
 }).listen(process.env.PORT);
